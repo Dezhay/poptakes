38405e343869df0611d4c33263d39570e903a632
Dezh <dezhaycaraway@gmail.com>
2019-04-05 16:43:27 -0700

    project init

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+const app = express();
+
+const PORT = process.env.PORT = 5000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log('Listening at:',PORT);
+});
